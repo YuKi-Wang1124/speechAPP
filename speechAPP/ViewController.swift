916a5fe36afa40e63ea72312b28f019d0c3fa4ae
yuki <wkiki1124@gmail.com>
2021-08-19 03:15:56 +0800

    commit

--- speechAPP/ViewController.swift
+++ speechAPP/ViewController.swift
@@ -6,14 +6,65 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var speechTextField: UITextField!
+
+    @IBOutlet weak var rateSlider: UISlider!
+
+    @IBOutlet weak var pitchSlider: UISlider!
+    
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    @IBOutlet weak var pitchLabel: UILabel!
+    
+    @IBOutlet weak var volumeLabel: UILabel!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    let synthesizer = AVSpeechSynthesizer()
+    var isPlaying = false
+    
+    @IBAction func speech(_ sender: Any) {
+        
+        let speechUtterance = AVSpeechUtterance(string: speechTextField.text!)
+        speechUtterance.voice = AVSpeechSynthesisVoice(language: "zh-TW")
+        
+       
+        speechUtterance.rate = rateSlider.value
+      
+        speechUtterance.pitchMultiplier = pitchSlider.value
+        
+        speechUtterance.volume = volumeSlider.value
+        
+        synthesizer.speak(speechUtterance)
+        
+        let speedNumber: String = String(format: "%.1f", rateSlider.value)
+        speedLabel.text = speedNumber
+        
+        let pitchNumber: String = String(format: "%.1f", pitchSlider.value)
+        pitchLabel.text = pitchNumber
+            
+        let volumeNumber: String = String(format: "%.1f", volumeSlider.value)
+        volumeLabel.text = volumeNumber
+        
+        isPlaying = true
+        }
+        
+    
+  
+    @IBAction func pauseButton(_ sender: Any) {
+        if isPlaying == false
+        {
+            return
+        }
+        
+        synthesizer.pauseSpeaking(at:AVSpeechBoundary.immediate)
+        
+        isPlaying = false
     }
-
-
+    
+    
 }
 
