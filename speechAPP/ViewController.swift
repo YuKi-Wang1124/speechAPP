8c8ea39b35a47b2064c4a00b3aa8c1dacbc56cc1
yuki <wkiki1124@gmail.com>
2021-08-19 00:23:05 +0800

    Initial Commit

---
+++ speechAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  speechAPP
+//
+//  Created by 王昱淇 on 2021/8/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ speechAPPUITests/speechAPPUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  speechAPPUITestsLaunchTests.swift
+//  speechAPPUITests
+//
+//  Created by 王昱淇 on 2021/8/19.
+//
+
+import XCTest
+
+class speechAPPUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
